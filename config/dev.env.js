f451b0ac625855e890223c0f111425598f6b6048
mintzhao <mint.zhao.chiu@gmail.com>
2018-11-24 02:19:29 +0800

    add teams & fix range value

--- config/dev.env.js
+++ config/dev.env.js
@@ -9,7 +9,7 @@ module.exports = merge(prodEnv, {
     HOST: '"api-kylin.eosasia.one"',
     PORT: '443',
     CHAINID: '"5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191"',
-    CONTRACT: '"aaasports111"'
+    CONTRACT: '"aaasports112"'
   }
 
 })
