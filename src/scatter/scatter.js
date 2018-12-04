8f17ce1d1fc1bfd05e571fdbae2b24920b38b223
mintzhao <mint.zhao.chiu@gmail.com>
2018-12-04 22:09:43 +0800

    catch user reject error

--- src/scatter/scatter.js
+++ src/scatter/scatter.js
@@ -34,32 +34,37 @@ export async function isscatterInstalled() {
 
 // get scatter eos
 export async function getScatterEOS(force = false) {
-  var scatter = store.getters.scatterEOS;
-  console.log('get scatter from store: ', scatter);
-  if (scatter == null || force) {
-    // for (var i = 0; i < 5; i++) {   await isscatterInstalled();   } const
-    // installed = await isscatterInstalled(); if (!installed) {   return scatter; }
-    // Don't forget to tell ScatterJS which plugins you are using.
-    ScatterJS.plugins(new ScatterEOS());
+  try {
+    var scatter = store.getters.scatterEOS;
+    console.log('get scatter from store: ', scatter);
+    if (scatter == null || force) {
+      // for (var i = 0; i < 5; i++) {   await isscatterInstalled();   } const
+      // installed = await isscatterInstalled(); if (!installed) {   return scatter; }
+      // Don't forget to tell ScatterJS which plugins you are using.
+      ScatterJS.plugins(new ScatterEOS());
 
-    const installed = await ScatterJS
-      .scatter
-      .connect(process.env.EOS.APPLICATION);
-    if (!installed) 
-      return store.getters.scatterEOS;
-    
-    scatter = ScatterJS.scatter;
-    const identity = await scatter.getIdentity(requiredFields);
-    console.log('await_get scatter from store: ', scatter, ', identity: ', identity);
-    if (identity) {
-      // store.dispatch('setScatterEOS', null);
-      store.dispatch('setScatterEOS', scatter);
+      const installed = await ScatterJS
+        .scatter
+        .connect(process.env.EOS.APPLICATION);
+      if (!installed) 
+        return store.getters.scatterEOS;
+      
+      scatter = ScatterJS.scatter;
+      const identity = await scatter.getIdentity(requiredFields);
+      console.log('await_get scatter from store: ', scatter, ', identity: ', identity);
+      if (identity) {
+        // store.dispatch('setScatterEOS', null);
+        store.dispatch('setScatterEOS', scatter);
+      }
+
+      window.ScatterJS = null;
     }
 
-    window.ScatterJS = null;
+    console.log('should show times as getPlayerIdentity');
+    console.log('getScatterEOS return: ', scatter);
+    return scatter;
+  } catch (error) {
+    console.log('getScatterEOS catch error: ', error);
+    return null;
   }
-
-  console.log('should show times as getPlayerIdentity');
-  console.log('getScatterEOS return: ', scatter);
-  return scatter;
 }
