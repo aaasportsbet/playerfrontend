66944e881fc916cfea023f74cc6c4b73d24dd703
mintzhao <mint.zhao.chiu@gmail.com>
2018-12-05 23:38:35 +0800

    add mainnet config

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,3 +1,13 @@
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  EOS: {
+    DEBUG: '"true"',
+    PROTOCOL: '"https"',
+    HOST: '"api-kylin.eosasia.one"',
+    PORT: '443',
+    CHAINID:
+        '"5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191"',
+    CONTRACTNBA: '"aaasports114"',
+    APPLICATION: '"aaasportsbet"'
+  }
 }
