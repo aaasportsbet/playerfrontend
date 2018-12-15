a7cd2268a9367f16d9fda105e9a9124221f1e57c
mintzhao <mint.zhao.chiu@gmail.com>
2018-12-15 21:14:07 +0800

    change stat logic

--- src/scatter/nba/bet.js
+++ src/scatter/nba/bet.js
@@ -23,34 +23,32 @@ export function playerBetWinStatus(bet) {
     default:
       return 'unknown';
   }
-}
+  }
 
 export function playerBetWinStatusLeft(round, bet) {
   if (round.result === bet.bet_val) {
     return 'win';
-  }
+    }
 
   return 'lose';
-}
+  }
 
 // get bet list
 export function getPlayerRoundBets(player, round) {
   console.log('getPlayerRoundBets player: ', player);
   if (player === '') {
     return [];
-  }
+    }
 
   var bets = [];
   if (round.bets) {
-    round
-      .bets
-      .forEach((bet) => {
-        if (bet.player != player || bet.round_id != round.id) {
-          return;
-        }
+    round.bets.forEach((bet) => {
+      if (bet.player != player || bet.round_id != round.id) {
+        return;
+      }
 
-        bets.push(bet);
-      });
+      bets.push(bet);
+    });
   }
 
   console.log('round bets: ', bets);
@@ -59,18 +57,16 @@ export function getPlayerRoundBets(player, round) {
 
 export function playerRoundJoinStatus(playerRoundBets, round) {
   switch (round.status) {
-    case 0: // betting
+    case 0:  // betting
       if (moment().unix() > round.bet_end_time) {
         return {index: 1, value: 'Playing'};
-      }
+        }
 
       return {
         index: 0,
-        value: playerRoundBets.length > 0
-          ? 'Join Again'
-          : 'Join Now'
+        value: playerRoundBets.length > 0 ? 'Join Again' : 'Join Now'
       };
-      // case 1:  // result published   return {index: 1, value: 'Playing'};
+    // case 1:  // result published   return {index: 1, value: 'Playing'};
     default:
       return {index: 2, value: 'View Detail'};
   }
@@ -80,22 +76,16 @@ export function roundBetValue(bet, round) {
   var winner;
   var points;
   switch (round.type) {
-    case 0: // pointdiff
-      winner = bet.bet_val > 0
-        ? round.hometeam
-        : round.awayteam;
+    case 0:  // pointdiff
+      winner = bet.bet_val > 0 ? round.hometeam : round.awayteam;
       points = Math.abs(bet.bet_val) + '';
       break;
-    case 1: // winlose
-      winner = bet.bet_val == 1
-        ? round.hometeam
-        : round.awayteam;
+    case 1:  // winlose
+      winner = bet.bet_val == 1 ? round.hometeam : round.awayteam;
       points = '';
       break;
-    case 2: // range
-      winner = bet.bet_val > 0
-        ? round.hometeam
-        : round.awayteam;
+    case 2:  // range
+      winner = bet.bet_val > 0 ? round.hometeam : round.awayteam;
       switch (Math.abs(bet.bet_val)) {
         case 1:
           points = '1~3';
@@ -112,23 +102,21 @@ export function roundBetValue(bet, round) {
         case 5:
           points = '20+';
           break;
-      }
+          }
       break;
-  }
+      }
 
   return {
     team_name: teamKeyShort[winner],
-    share: calcBetTotal(round.bet_unit, bet.share, false, 2),
-    team_score: points,
-    user_win_status: playerBetWinStatus(bet)
+        share: calcBetTotal(round.bet_unit, bet.share, false, 2),
+        team_score: points, user_win_status: playerBetWinStatus(bet)
   }
 };
 
-export function getPlayerRoundBetLatest(playerRoundBets, round) {
-  return playerRoundBets.length > 0
-    ? roundBetValue(playerRoundBets[0], round)
-    : 0
-};
+export function getPlayerRoundBetLatest(
+    playerRoundBets, round){return playerRoundBets.length > 0 ?
+                                roundBetValue(playerRoundBets[0], round) :
+                                0};
 
 export function getPlayerPreviousRoundBets(playerRoundBets, round) {
   var bets = [];
@@ -156,81 +144,80 @@ export async function getPlayerBetStatEOS(player) {
   if (scatter != null && scatter.identity) {
     const eos = scatter.eos(network, Eos, eosOptions);
     const tablekey = new BigNumber(Eos.modules.format.encodeName(player, false))
-    const result = await eos.getTableRows({
-      json: true,
-      code: contract,
-      scope: contract,
-      table: 'betstateoss',
-      lower_bound: tablekey.toString(),
-      upper_bound: tablekey
-        .plus(1)
-        .toString(),
-      limit: 1
-    });
+        const result = await eos.getTableRows({
+          json: true,
+          code: contract,
+          scope: contract,
+          table: 'playerstats',
+          lower_bound: tablekey.toString(),
+          upper_bound: tablekey.plus(1).toString(),
+          limit: 1
+        });
 
     console.log('player bet stat response: ', result);
     if (result.rows.length > 0) {
       const retstat = result.rows[0]
 
-      stat = {
-        player: player,
-        join_times: retstat.join_times,
-        win_times: retstat.win_times,
-        bet_amount: asset2amount(retstat.bet_amount, 0),
-        win_amount: asset2amount(retstat.win_amount, 0),
-        extras: []
-      };
+      retstat.stats.forEach(s => {
+        if (asset_symbol(s.assettype) === 'EOS') {
+          stat = {
+            player: player,
+            join_times: s.join_times,
+            win_times: s.win_times,
+            bet_amount: asset2amount(s.bet_amount, 0),
+            win_amount: asset2amount(s.win_amount, 0),
+            extras: []
+          };
+        }
+      });
     }
   }
 
   console.log('player stat: ', stat);
   return stat;
-}
+  }
 
 /// bet round
-export async function betRound(playerIdentity, round, winner, range, points, shares) {
+export async function betRound(
+    playerIdentity, round, winner, range, points, shares) {
   console.log('bet round identity: ', playerIdentity);
 
   if (!playerIdentity) {
     return {errno: 401, error: 'player not login'};
-  }
+    }
 
   let homewin = false;
   if (round.hometeam == winner) {
     homewin = true;
   } else {
     homewin = false;
-  }
+    }
 
   let betval = '';
   switch (round.type) {
     case 0:
-      betval = homewin
-        ? points.toString()
-        : (-points).toString();
+      betval = homewin ? points.toString() : (-points).toString();
       break;
     case 1:
-      betval = homewin
-        ? 1 + ''
-        : 0 + '';
+      betval = homewin ? 1 + '' : 0 + '';
       break;
     case 2:
-      betval = homewin
-        ? range.toString()
-        : (-range).toString();
+      betval = homewin ? range.toString() : (-range).toString();
       break;
-  }
+      }
 
   const quant = calcBetTotal(round.bet_unit, shares);
   if (quant == '') {
     return {errno: 400, error: 'quantity is invalid'};
-  }
+    }
 
   try {
     const scatter = await getScatterEOS();
     if (scatter != null && scatter.identity) {
       const eos = scatter.eos(network, Eos, eosOptions);
-      const response = await eos.transfer(`${playerIdentity.name}`, contract, quant, 'bet|' + round.id + ',' + betval);
+      const response = await eos.transfer(
+          `${playerIdentity.name}`, contract, quant,
+          'bet|' + round.id + ',' + betval);
       return {errno: 200, data: response};
     } else {
       return {errno: 401, error: 'player not login'};
@@ -240,16 +227,17 @@ export async function betRound(playerIdentity, round, winner, range, points, sha
   }
 };
 
-export function calcBetTotal(round_bet_unit, shares, original = true, fixed = 4) {
+export function calcBetTotal(
+    round_bet_unit, shares, original = true, fixed = 4) {
   const units = round_bet_unit.split(' ', 2);
   if (units.length != 2) {
     return '';
-  }
+    }
 
   const unit = parseFloat(units[0]);
   if (unit == 0) {
     return '';
-  }
+    }
 
   const symbol = units[1];
   if (original) {
@@ -258,7 +246,7 @@ export function calcBetTotal(round_bet_unit, shares, original = true, fixed = 4)
       case 'EOS':
         return DecimalPad(unit * shares, 4) + ' ' + symbol;
     }
-  }
+    }
 
   return (unit * shares).toFixed(fixed) + ' ' + symbol;
 };
@@ -267,12 +255,21 @@ export function asset2amount(asset, fixed = 0) {
   const units = asset.split(' ', 2);
   if (units.length != 2) {
     return 0;
-  }
+    }
 
   const unit = parseFloat(units[0]);
   if (unit == 0) {
     return 0;
-  }
+    }
 
   return unit.toFixed(fixed);
+  }
+
+function asset_symbol(asset) {
+  const units = asset.split(' ', 2);
+  if (units.length != 2) {
+    return '';
+    }
+
+  return units[1];
 }
