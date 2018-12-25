233acfe5971b2cb497e0133410a52fa2d558a784
mintzhao <mint.zhao.chiu@gmail.com>
2018-12-25 23:07:51 +0800

    update config

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,9 +2,9 @@ module.exports = {
   NODE_ENV: '"production"',
   EOS: {
     DEBUG: '"true"',
-    PROTOCOL: '"http"',
-    HOST: '"api-mainnet.starteos.io"',
-    PORT: '80',
+    PROTOCOL: '"https"',
+    HOST: '"api.eoslaomao.com"',
+    PORT: '443',
     CHAINID: '"aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"',
     CONTRACTNBA: '"aaasportsnba"',
     APPLICATION: '"aaasportsbet"'
