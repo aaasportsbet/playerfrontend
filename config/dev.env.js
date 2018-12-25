06dedc418813d4452944093dfe84f2b35d76ac17
mintzhao <mint.zhao.chiu@gmail.com>
2018-12-25 23:02:55 +0800

    update config

--- config/dev.env.js
+++ config/dev.env.js
@@ -8,8 +8,9 @@ module.exports = merge(prodEnv, {
     PROTOCOL: '"https"',
     HOST: '"api-kylin.eosasia.one"',
     PORT: '443',
-    CHAINID: '"5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191"',
-    CONTRACTNBA: '"aaasports114"',
+    CHAINID:
+        '"5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191"',
+    CONTRACTNBA: '"aaasports115"',
     APPLICATION: '"aaasportsbet"'
   }
 
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,12 +2,11 @@ module.exports = {
   NODE_ENV: '"production"',
   EOS: {
     DEBUG: '"true"',
-    PROTOCOL: '"https"',
-    HOST: '"api-kylin.eosasia.one"',
-    PORT: '443',
-    CHAINID:
-        '"5fff1dae8dc8e2fc4d5b23b2c7665c97f9e9d8edf2b6485a86ba311c25639191"',
-    CONTRACTNBA: '"aaasports114"',
+    PROTOCOL: '"http"',
+    HOST: '"api-mainnet.starteos.io"',
+    PORT: '80',
+    CHAINID: '"aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906"',
+    CONTRACTNBA: '"aaasportsnba"',
     APPLICATION: '"aaasportsbet"'
   }
 }
